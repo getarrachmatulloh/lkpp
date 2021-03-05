238c958a362cdc7e2c4bfafc737888f7903a6c0e
Getar Rachmatulloh <67212058+getarrachmatulloh@users.noreply.github.com>
2021-03-05 15:56:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>LKPP</title>
+  </head>
+  <body>
+  <div class="container">
+		<div class="row">
+			<div class="col-sm-7">
+				<h1>CEK PAKET</h1><br>
+				<div class="input-group mb-3">	
+					<div class="col-sm-8">				
+						<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Input resi anda ..."></input>
+					</div>
+					<div class="col-sm-1">
+					</div>
+					<div class="col-sm-3">
+						<button class="form-control btn btn-primary" type="submit">Cari</button>
+					</div>
+				</div>
+				<div class="input-group mb-3">	
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+					  <label class="form-check-label" for="inlineRadio1">JNE</label>
+					</div>&nbsp;&nbsp;&nbsp;
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					  <label class="form-check-label" for="inlineRadio2">TIKI</label>
+					</div>&nbsp;&nbsp;&nbsp;
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					  <label class="form-check-label" for="inlineRadio2">POS</label>
+					</div>&nbsp;&nbsp;&nbsp;
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					  <label class="form-check-label" for="inlineRadio2">SiCepat</label>
+					</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					  <label class="form-check-label" for="inlineRadio2">AntarAja</label>
+					</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					  <label class="form-check-label" for="inlineRadio2">Ninja</label>
+					</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					  <label class="form-check-label" for="inlineRadio2">JNT</label>
+					</div>
+				</div>			
+				<p>DELIVERED TO ......</p>
+				<hr>
+				<table class="table table-borderless">
+					<tr>
+						<th>Nomor Resi</th>
+						<th>Tanggal Pengiriman</th>
+						<th>Pengirim</th>
+						<th>Penerima</th>
+					</tr>
+					<tr>
+						<td>......</td>
+						<td>......</td>
+						<td>......</td>
+						<td>......</td>
+					</tr>
+				</table>
+				<hr>
+				<p><b>Detil Pengiriman</b></p>
+				<table class="table table-borderless">
+					<tr>
+						<th width="35%">Tanggal/Waktu</th>
+						<th width="65%">Deskripsi</th>					
+					</tr>
+					<tr>
+						<td>......</td>
+						<td>......</td>				
+					</tr>
+				</table>
+			</div>			
+			<div class="col-sm-2">
+			</div>
+			<div class="col-sm-3">
+				<p style="margin-top: 100%;"><b>Respons Toko</b><br>
+				......</p>
+				<p style='color:white'><span  style="background-color:#0f0; padding:10px; border-radius: 5px;" > .................. </span></p>
+			</div>
+		</div>
+	</div>
+
+	
+	
+	
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  </body>
+</html>
